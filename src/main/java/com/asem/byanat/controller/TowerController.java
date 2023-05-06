4842328c120151bc4395f54964c04ddb2a1c9376
asemivu13 <asemivu13@gmail.com>
2023-05-06 09:44:57 +0300

    Add validation for user input filtration parameters

--- src/main/java/com/asem/byanat/controller/TowerController.java
+++ src/main/java/com/asem/byanat/controller/TowerController.java
@@ -2,6 +2,8 @@ package com.asem.byanat.controller;
 
 import com.asem.byanat.model.Tower;
 import com.asem.byanat.service.TowerService;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,8 +11,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/challenge")
+@Validated
 public class TowerController {
-    private final TowerService towerService;
+    private TowerService towerService;
 
     public TowerController(TowerService towerService) {
         this.towerService = towerService;
@@ -20,7 +23,7 @@ public class TowerController {
     public @ResponseBody List<Tower> getTowers(
             @RequestParam(value = "networkoperator", required = false) String networkOperator,
             @RequestParam(value = "technology", required = false) String technology,
-            @RequestParam(value = "towerid", required = false) String towerId,
+            @RequestParam(value = "towerid", required = false) @Pattern(regexp = "\\d+", message = "Only numbers are allowed") String towerId,
             @RequestParam(value = "towertype", required = false) String towerType
     ) {
         return towerService.getTowers(networkOperator, technology, towerId, towerType);
