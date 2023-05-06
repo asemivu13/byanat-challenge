70b6f6ecbd6269f619f25138121104bdf79a3c45
asemivu13 <asemivu13@gmail.com>
2023-05-06 09:07:34 +0300

    Initial commit

---
+++ src/main/java/com/asem/byanat/ByanatApplication.java
@@ -0,0 +1,13 @@
+package com.asem.byanat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByanatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByanatApplication.class, args);
+	}
+
+}
